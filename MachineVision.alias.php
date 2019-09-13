ac22dd8388eb2890d1d3eb3edff73cd95a89e2b0
Zoranzoki21 <zorandori4444@gmail.com>
2019-09-14 00:38:53 +0200

    Add aliases for some languages
    Change-Id: I2daef64f439125424141e36e51e6a586cc0cbfe5

--- MachineVision.alias.php
+++ MachineVision.alias.php
@@ -2,6 +2,32 @@
 
 $specialPageAliases = [];
 
+/* English */
 $specialPageAliases['en'] = [
 	'SuggestedTags' => [ 'SuggestedTags' ],
+];
+
+/* Bosnian (bosanski) */
+$specialPageAliases['bs'] = [
+	'SuggestedTags' => [ 'Predložene_oznake' ],
+];
+
+/* Croatian (hrvatski) */
+$specialPageAliases['hr'] = [
+	'SuggestedTags' => [ 'Predložene_oznake' ],
+];
+
+/* Serbo-Crotian (srpsko-hrvatski) */
+$specialPageAliases['sh'] = [
+	'SuggestedTags' => [ 'Predložene_oznake' ],
+];
+
+/* Serbian Cyrillic (српски (ћирилица)) */
+$specialPageAliases['sr-ec'] = [
+	'SuggestedTags' => [ 'Предложене_ознаке' ],
+];
+
+/* Serbian Latin (srpski (latinica)) */
+$specialPageAliases['sr-el'] = [
+	'SuggestedTags' => [ 'Predložene_oznake' ],
 ];
