f348b721203b396e418b1adcd616b79e070db2a0
annet <atomasevich@wikimedia.org>
2019-10-02 16:27:53 -0400

    Enable keyboard navigation and refactor to single SuggestionWidget
    In addition to enabling focus and enter keypress for tags, this
    patch refacotrs SuggestionBaseWidget, SuggestionConfirmedWidget, and
    SuggestionWidget into a single component, using a config var to
    determine whether or not the suggestion has been confirmed.
    
    Bug: T234357
    Change-Id: I1ce06c2dede23a7e06d0eb1dc5689f6acacc5323

--- tests/node-qunit/widgets/SuggestionWidget.test.js
+++ tests/node-qunit/widgets/SuggestionWidget.test.js
@@ -9,3 +9,83 @@ QUnit.test( 'Constructor test', function ( assert ) {
 		widget = new SuggestionWidget( { suggestionData: data } );
 	assert.ok( true );
 } );
+
+QUnit.test( 'Unconfirmed widget emits confirmSuggestion event', function ( assert ) {
+	var SuggestionWidget = require( pathToWidget ),
+		data = { text: 'Test label' },
+		widget = new SuggestionWidget( {
+			suggestionData: data,
+			confirmed: false
+		} ),
+		done = assert.async();
+
+	widget.on( 'confirmSuggestion', function () {
+		assert.ok( true );
+	} );
+
+	widget.toggleSuggestion();
+
+	setTimeout( function () {
+		done();
+	}, 100 );
+} );
+
+QUnit.test( 'Confirmed widget emits unconfirmSuggestion event', function ( assert ) {
+	var SuggestionWidget = require( pathToWidget ),
+		data = { text: 'Test label' },
+		widget = new SuggestionWidget( {
+			suggestionData: data,
+			confirmed: true
+		} ),
+		done = assert.async();
+
+	widget.on( 'unconfirmSuggestion', function () {
+		assert.ok( true );
+	} );
+
+	widget.toggleSuggestion();
+
+	setTimeout( function () {
+		done();
+	}, 100 );
+} );
+
+QUnit.test( 'Enter keypress on unconfirmed widget emits confirmSuggestion event', function ( assert ) {
+	var SuggestionWidget = require( pathToWidget ),
+		data = { text: 'Test label' },
+		widget = new SuggestionWidget( {
+			suggestionData: data,
+			confirmed: false
+		} ),
+		done = assert.async();
+
+	widget.on( 'confirmSuggestion', function () {
+		assert.ok( true );
+	} );
+
+	widget.onKeypress( { keyCode: 13 } );
+
+	setTimeout( function () {
+		done();
+	}, 100 );
+} );
+
+QUnit.test( 'Enter keypress on confirmed widget emits unconfirmSuggestion event', function ( assert ) {
+	var SuggestionWidget = require( pathToWidget ),
+		data = { text: 'Test label' },
+		widget = new SuggestionWidget( {
+			suggestionData: data,
+			confirmed: true
+		} ),
+		done = assert.async();
+
+	widget.on( 'unconfirmSuggestion', function () {
+		assert.ok( true );
+	} );
+
+	widget.onKeypress( { keyCode: 13 } );
+
+	setTimeout( function () {
+		done();
+	}, 100 );
+} );
