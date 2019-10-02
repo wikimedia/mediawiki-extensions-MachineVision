fa0d3ccb60f871999c58b8448dd9d24f028a5a66
Michael Holloway <mholloway@wikimedia.org>
2019-10-02 12:27:23 -0400

    Add script to create file lists based on global image usage
    Adds a script to create file lists based on global image usage. The
    minimum number of external usages and the namespaces to be considered
    are passed in as parameters (with NS_MAIN as the default for the
    latter).
    
    Filtering is applied as with the script to create lists based on
    categories and templates.
    
    Bug: T234250
    Change-Id: I76d5757abf7e2052eddd34791bc12b021930d201

---
+++ maintenance/createFileListFromGlobalImageLinks.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace MediaWiki\Extension\MachineVision\Maintenance;
+
+use Maintenance;
+use MediaWiki\Extension\MachineVision\Services;
+use MediaWiki\Extension\MachineVision\TitleFilter;
+use MediaWiki\MediaWikiServices;
+use RepoGroup;
+use Wikimedia\Rdbms\IDatabase;
+
+$basePath = getenv( 'MW_INSTALL_PATH' ) !== false
+	? getenv( 'MW_INSTALL_PATH' )
+	: __DIR__ . '/../../..';
+require_once "$basePath/maintenance/Maintenance.php";
+
+/**
+ * Create a list of files for which to request image labels based on a minimum number of incoming
+ * image links. The resulting list is formatted for passing as input to fetchSuggestions.php.
+ */
+class CreateFileListFromGlobalImageLinks extends Maintenance {
+
+	/** @var RepoGroup */
+	private $repoGroup;
+
+	/** @var TitleFilter */
+	private $titleFilter;
+
+	/** @var IDatabase */
+	private $dbr;
+
+	/** @var int */
+	private $minImageWidth;
+
+	/** @var string[] */
+	private $categoryBlacklist;
+
+	/** @var string[] */
+	private $templateBlacklist;
+
+	public function __construct() {
+		parent::__construct();
+		$this->requireExtension( 'MachineVision' );
+		$this->addDescription( 'Creates a list of files for which to request image labels based '
+			. 'on their total incoming image links, formatted for passing as input to '
+			. 'fetchSuggestions.php' );
+		$this->addOption( 'minLinks', 'Minimum incoming links for a file to be added',
+			true, true, 'm' );
+		$this->addOption( 'namespace', 'Count incoming links for pages in this namespace '
+			. '(default: NS_MAIN)',
+			false, true, 'n' );
+		$this->addOption( 'outputFile', 'Filename to which to write results',
+			true, true, 'o' );
+	}
+
+	public function init() {
+		$services = MediaWikiServices::getInstance();
+		$extensionServices = new Services( $services );
+		$this->repoGroup = $services->getRepoGroup();
+		$this->titleFilter = TitleFilter::instance();
+
+		$extensionConfig = $extensionServices->getExtensionConfig();
+		$this->minImageWidth = $extensionConfig->get( 'MachineVisionMinImageWidth' );
+		$this->categoryBlacklist = $extensionConfig->get( 'MachineVisionCategoryBlacklist' );
+		$this->templateBlacklist = $extensionConfig->get( 'MachineVisionTemplateBlacklist' );
+
+		$loadBalancerFactory = $services->getDBLoadBalancerFactory();
+
+		$cluster = $extensionConfig->get( 'MachineVisionCluster' );
+		$database = $extensionConfig->get( 'MachineVisionDatabase' );
+
+		$loadBalancer = $cluster
+			? $loadBalancerFactory->getExternalLB( $cluster )
+			: $loadBalancerFactory->getMainLB( $database );
+
+		$this->dbr = $loadBalancer->getLazyConnectionRef( DB_REPLICA, [], $database );
+	}
+
+	/** @inheritDoc */
+	public function execute() {
+		$this->init();
+		$minLinks = $this->getOption( 'minLinks' );
+		$namespace = $this->getOption( 'namespace' ) ?: NS_MAIN;
+		$outputFile = $this->getOption( 'outputFile' );
+		$result = [];
+
+		$query = $this->dbr->select(
+			'globalimagelinks',
+			[ 'gil_to', 'COUNT(*)' ],
+			[ 'gil_page_namespace_id' => $namespace ],
+			__METHOD__,
+			[ 'GROUP BY gil_to', "HAVING COUNT(*) >= $minLinks" ]
+		);
+
+		foreach ( $query as $row ) {
+			$title = $row->gil_to;
+			if ( $this->titleFilter->isGoodTitle(
+				$title,
+				$this->repoGroup->getLocalRepo(),
+				$this->minImageWidth,
+				$this->categoryBlacklist,
+				$this->templateBlacklist
+			) ) {
+				$result[] = $title;
+			}
+		}
+
+		file_put_contents( $outputFile, array_map( function ( $title ) {
+			return "$title\n";
+		}, $result ) );
+	}
+
+}
+
+$maintClass = CreateFileListFromGlobalImageLinks::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
