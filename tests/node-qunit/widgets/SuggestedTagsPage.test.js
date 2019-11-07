df642b4ae6807d0c8f5d2a8ed1a0df9e89dbf6b5
annet <atomasevich@wikimedia.org>
2019-11-07 12:44:39 -0500

    Test SuggestedTagsPage widget
    Bug: T236827
    Change-Id: I6a0d50d594aa7543c0c1a782be19044ff243b4ce

--- tests/node-qunit/widgets/SuggestedTagsPage.test.js
+++ tests/node-qunit/widgets/SuggestedTagsPage.test.js
@@ -1,6 +1,7 @@
 var pathToWidget = '../../../resources/widgets/SuggestedTagsPage.js',
 	hooks = require( '../support/hooks.js' ),
-	sinon = require( 'sinon' );
+	sinon = require( 'sinon' ),
+	sandbox;
 
 QUnit.module( 'SuggestedTagsPage', hooks );
 
@@ -36,21 +37,71 @@ QUnit.test( 'Tabs do not load for un-autoconfirmed user', function ( assert ) {
 	assert.strictEqual( widget.tabs, undefined );
 } );
 
+QUnit.module( 'SuggestedTagsPage with valid user', {
+	beforeEach: function () {
+		sandbox = sinon.createSandbox();
+		hooks.beforeEach();
+
+		// Configure a logged in, autoconfirmed user.
+		sandbox.stub( global.mw.config, 'get' )
+			.withArgs( 'wgUserName' ).returns( 'UserName' )
+			.withArgs( 'wgUserGroups' ).returns( [ '*', 'user', 'autoconfirmed' ] );
+
+		// Stub out mw.Api.get method so we can test fetching items.
+		global.mw.Api = function () {};
+		global.mw.Api.prototype = {
+			get: sandbox.stub().returns( $.Deferred().resolve( {} ).promise() )
+		};
+
+		global.OO.ui.WindowManager.prototype.openWindow = sandbox.stub();
+	},
+	afterEach: function () {
+		hooks.afterEach();
+		sandbox.restore();
+	}
+} );
+
 QUnit.test( 'Tabs load for authenticated and autoconfirmed user', function ( assert ) {
 	var SuggestedTagsPage = require( pathToWidget ),
-		widget,
-		sandbox = sinon.createSandbox();
+		widget;
 
-	sandbox.stub( global.mw.config, 'get' )
-		.withArgs( 'wgUserName' ).returns( 'UserName' )
-		.withArgs( 'wgUserGroups' ).returns( [ '*', 'user', 'autoconfirmed' ] );
-
-	// Stub the API object and return an empty object as a response.
-	global.mw.Api = function () {};
-	global.mw.Api.prototype = {
-		get: sinon.stub().returns( $.Deferred().resolve( {} ).promise() )
-	};
-
-	widget = new SuggestedTagsPage();
+	widget = new SuggestedTagsPage( { startTab: 'popular' } );
 	assert.strictEqual( widget.tabs instanceof OO.ui.IndexLayout, true );
+} );
+
+QUnit.test( '"#popular" fragment in URL results in active popular tab on page load', function ( assert ) {
+	var SuggestedTagsPage = require( pathToWidget ),
+		widget;
+
+	widget = new SuggestedTagsPage( { startTab: 'popular' } );
+	assert.strictEqual( widget.tabs.getCurrentTabPanelName(), 'popular' );
+} );
+
+QUnit.test( '"#user" fragment in URL results in active user tab on page load', function ( assert ) {
+	var SuggestedTagsPage = require( pathToWidget ),
+		widget;
+
+	widget = new SuggestedTagsPage( { startTab: 'user' } );
+	assert.strictEqual( widget.tabs.getCurrentTabPanelName(), 'user' );
+} );
+
+QUnit.test( 'Hash change to existing tab sets that tab as active', function ( assert ) {
+	var SuggestedTagsPage = require( pathToWidget ),
+		widget;
+
+	widget = new SuggestedTagsPage( { startTab: 'user' } );
+	widget.onHashChange( { newURL: 'https://example.com/Special:SuggestedTags#popular' } );
+	assert.strictEqual( widget.tabs.getCurrentTabPanelName(), 'popular' );
+
+	widget.onHashChange( { newURL: 'https://example.com/Special:SuggestedTags#user' } );
+	assert.strictEqual( widget.tabs.getCurrentTabPanelName(), 'user' );
+} );
+
+QUnit.test( 'Hash change to nonexistant tab sends user to popular tab', function ( assert ) {
+	var SuggestedTagsPage = require( pathToWidget ),
+		widget;
+
+	widget = new SuggestedTagsPage( { startTab: 'user' } );
+	widget.onHashChange( { newURL: 'https://example.com/Special:SuggestedTags#asdf' } );
+	assert.strictEqual( widget.tabs.getCurrentTabPanelName(), 'popular' );
 } );
