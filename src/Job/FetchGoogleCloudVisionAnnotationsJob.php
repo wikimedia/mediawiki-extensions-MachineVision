316cf6afdd7e64e7f57ff4eb15b774897fe53ad0
Michael Holloway <mholloway@wikimedia.org>
2019-12-13 18:20:27 -0500

    Fix: Bail out of label fetching job if local file not found
    A configurable delay is applied to annotation fetching jobs due to the
    high likelihood of newly uploaded files being deleted. However, the job
    currently throws an error due to the assumption that the file will be
    found locally. This updates the job to log the file title and bail out
    in the event that the file is not found at job execution time.
    
    Bug: T240733
    Change-Id: Ic861280e889f128fd108f35a165d4f3be2ca90ae

--- src/Job/FetchGoogleCloudVisionAnnotationsJob.php
+++ src/Job/FetchGoogleCloudVisionAnnotationsJob.php
@@ -4,14 +4,31 @@ namespace MediaWiki\Extension\MachineVision\Job;
 
 use Job;
 use MediaWiki\Extension\MachineVision\Services;
+use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
+use MWException;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Title;
 
-class FetchGoogleCloudVisionAnnotationsJob extends Job {
+class FetchGoogleCloudVisionAnnotationsJob extends Job implements LoggerAwareInterface {
+
+	use LoggerAwareTrait;
+
+	/**
+	 * FetchGoogleCloudVisionAnnotationsJob constructor.
+	 * @param string $command
+	 * @param array|Title|null $params
+	 */
+	public function __construct( string $command, $params = null ) {
+		parent::__construct( $command, $params );
+		$this->setLogger( LoggerFactory::getInstance( 'machinevision' ) );
+	}
 
 	/**
 	 * @return bool success
 	 * @suppress PhanTypeMismatchArgument
+	 * @throws MWException
 	 */
 	public function run(): bool {
 		$title = $this->params['title'];
@@ -21,6 +38,14 @@ class FetchGoogleCloudVisionAnnotationsJob extends Job {
 		$services = MediaWikiServices::getInstance();
 		$repoGroup = $services->getRepoGroup();
 		$file = $repoGroup->getLocalRepo()->findFile( Title::newFromText( $title, $namespace ) );
+
+		if ( !$file ) {
+			// File not found. Note that this is an expected scenario. The extension
+			// configuration provides for delaying this job due to the high likelihood of newly
+			// uploaded files being deleted.
+			$this->logger->info( 'Local file not found', [ 'title' => $title ] );
+			return true;
+		}
 
 		$extensionServices = new Services( $services );
 		$client = $extensionServices->getGoogleCloudVisionClient();
