e224f06f1b006a3563902b70d54023936829783e
annet <atomasevich@wikimedia.org>
2020-04-15 14:18:29 -0400

    Show focus feedback on suggestions
    When navigating via keyboard, focus lands on the (visually-hidden)
    checkbox element. This patch adds focusin and focusout handlers to
    toggle a class on the suggestion to show a border and box shadow
    when the checkbox is in focus.
    
    Bug: T249932
    Change-Id: I096bd79475efbef02dd0db5da9bccf46e3710215

--- resources/widgets/SuggestionsWidget.js
+++ resources/widgets/SuggestionsWidget.js
@@ -33,20 +33,41 @@ OO.inheritClass( SuggestionsWidget, OO.ui.CheckboxMultiselectWidget );
  */
 SuggestionsWidget.prototype.createOption = function ( suggestion ) {
 	var option = new OO.ui.CheckboxMultioptionWidget( {
-		data: suggestion.wikidataId,
-		label: suggestion.text,
-		classes: [ 'wbmad-suggestions__suggestion' ]
-	} );
+			data: suggestion.wikidataId,
+			label: suggestion.text,
+			classes: [ 'wbmad-suggestions__suggestion' ]
+		} ),
+		self = this;
 
 	// Swap out classes so we can style things ourselves.
 	option.checkbox.$element
 		.removeClass( 'oo-ui-checkboxInputWidget' )
-		.addClass( 'wbmad-suggestions__suggestion__checkbox-widget' );
+		.addClass( 'wbmad-suggestions__suggestion__checkbox-widget' )
+		.on( {
+			focusin: self.onCheckboxFocusIn.bind( self, option ),
+			focusout: self.onCheckboxFocusOut.bind( self, option )
+		} );
 	option.checkbox.checkIcon.$element
 		.removeClass( 'oo-ui-checkboxInputWidget-checkIcon oo-ui-image-invert' )
 		.addClass( 'wbmad-suggestions__suggestion__check-icon' );
 
 	return option;
+};
+
+/**
+ * Add class for option focus styles.
+ * @param {OO.ui.CheckboxMultioptionWidget} option
+ */
+SuggestionsWidget.prototype.onCheckboxFocusIn = function ( option ) {
+	option.$element.addClass( 'wbmad-suggestions__suggestion--focus' );
+};
+
+/**
+ * Remove class for option focus styles.
+ * @param {OO.ui.CheckboxMultioptionWidget} option
+ */
+SuggestionsWidget.prototype.onCheckboxFocusOut = function ( option ) {
+	option.$element.removeClass( 'wbmad-suggestions__suggestion--focus' );
 };
 
 module.exports = SuggestionsWidget;
