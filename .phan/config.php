7ca0a574a0c657c7d21eaa6c34bf3a03d403c9f7
Umherirrender <umherirrender_de.wp@web.de>
2021-09-05 12:40:23 +0200

    build: Fix phan config to pass
    Change-Id: Ib097c89c5bb8a38041fe8b3da89d898a030e6824

--- .phan/config.php
+++ .phan/config.php
@@ -5,6 +5,7 @@ $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
 		'../../extensions/Echo',
+		'../../extensions/Wikibase/lib',
 	]
 );
 
@@ -12,6 +13,7 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
 		'../../extensions/Echo',
+		'../../extensions/Wikibase/lib',
 	]
 );
 
